f11ca3162b95dafb9d87b62b94805162ffbc936d
David <jamesdwallace55@gmail.com>
2018-11-02 16:26:29 -0600

    Summary
    Adding code and project settings to this thing. Not quite sure I'm doing this correctly...

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Nov 2, 2018
+ *      Author: jdw
+ */
+
+
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
+
